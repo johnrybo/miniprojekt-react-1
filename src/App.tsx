0b84b1774fcf0b3eb2917e28255a6162ebd27e9a
lsoder <lindaevasoder@gmail.com>
2021-02-23 11:14:44 +0100

    add some styling

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import './App.css';
 
 //import components
-import Header from './components/header';
+import Header from './components/Header';
 
 function App() {
   return (
--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,8 +1,9 @@
 import React from 'react';
+import '../App.css';
 
 function Header() {
     return (
-        <h1>Miniprojekt React 1</h1>
+        <h1>Miniprojekt React 1!!</h1>
     )
 }
 
