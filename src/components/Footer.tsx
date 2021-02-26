0e957df0d2664e812acb5af45e13ef06d60981fa
lsoder <lindaevasoder@gmail.com>
2021-02-26 10:17:09 +0100

    change and adding files

---
+++ src/components/Footer.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../App.css';
+
+function Footer() {
+    return(
+        <div>
+            Detta är footern!
+        </div>
+    )
+}
+
+export default Footer;
---
+++ src/components/Main.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import '../App.css';
+
+//import components
+import Weather from './Weather';
+
+function Main() {
+    return(
+        <div>
+            <Weather />
+        </div>
+    )
+
+}
+
+export default Main;
