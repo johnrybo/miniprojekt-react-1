c934d3a0fc081747c287916bace9734346718310
miaherman <mia.herman@live.se>
2021-02-26 09:45:53 +0100

    ändrat ii header

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import './App.css';
 
 //import components
-import Header from './components/Header';
+import Header from './components/header'
 import Weather from './components/Weather';
 
 function App() {
