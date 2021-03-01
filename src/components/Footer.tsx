bb7abf79f1fd8947cec4fb990d9128400306923f
miaherman <mia.herman@live.se>
2021-03-01 21:22:52 +0100

    lil logo

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,12 +1,20 @@
-import React from 'react';
+import React, { CSSProperties } from 'react';
 import '../App.css';
+import logo from './assets/logo.png';
+
 
 function Footer() {
     return(
         <footer>
-            <p>© JML 2021🌤</p>
+            <text>
+                <img style={logoStyle} src={logo} alt="logo"></img>
+                &nbsp; 
+            <p> ©  2021</p>
+            </text>
         </footer>
     )
 }
-
+const logoStyle: CSSProperties = {
+    width: '3rem'
+}
 export default Footer;
